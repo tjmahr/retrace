25ef515936ba6e2bf2a15917327e43b9bb66b0af
TJ Mahr <mahr@wisc.edu>
2014-12-02 21:26:26 -0600

    trace parameters

---
+++ R/trace.R
@@ -0,0 +1,16 @@
+trace_parameters <- list(
+  # bottom-up excitation
+  excite_feat_phon = .02,
+  excite_phon_word = .05,
+  # top-down excitation
+  excite_word_phon = .03,
+  excite_phon_feat = .00,
+  # lateral inhibition
+  inhibit_feat = .04,
+  inhibit_phon = .04,
+  inhibit_word = .04,
+  # decay
+  decay_feat = .01,
+  decay_phon = .03,
+  decay_word = .05
+)
